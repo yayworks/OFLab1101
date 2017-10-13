563e2a0817031c79f6939173ac71b4c14f8f03ec
yayworks <raj@yayworks.com>
2017-10-13 07:10:30 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd /data
+#cd /data
 
-exec /usr/local/bin/nimbix_desktop /opt/paraviewopenfoam54/bin/paraview
+#exec /usr/local/bin/nimbix_desktop /opt/paraviewopenfoam54/bin/paraview
 #exec /usr/local/bin/nimbix_desktop /usr/local/ParaView-5.4.1/bin/paraview
+
+cd /home/nimbix
+exec mkdir OpenFOAM
+
 
