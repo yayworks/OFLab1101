e9d8a72650547a8846771219f6688cbc2bb09951
yayworks <raj@yayworks.com>
2017-10-13 07:14:22 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,5 +7,10 @@
 
 cd /home/nimbix
 exec mkdir OpenFOAM
+exec source /opt/openfoam4/etc/bashrc
+cd OpenFOAM
+exec mkdir -p $FOAM_RUN
+cd nimbix-4.1/run
+exec /usr/local/bin/nimbix_desktop /opt/paraviewopenfoam4/bin/paraview
 
 
